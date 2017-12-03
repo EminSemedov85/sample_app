3ac9a46c678233880e5645c8974285e989c0829c
Emin Semedov <milli.emin@yahoo.com>
2017-12-03 02:53:55 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  
+  def hello
+    render html: "Oruc senden yoxdur, 1 ile milyonersen!!!!!"
+  end 
+
 end
